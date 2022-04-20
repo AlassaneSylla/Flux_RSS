7ddbebeb10d47f772f251f482e38659c14aaa5ee
PapiAlou <papialou90@gmail.com>
2022-04-20 07:55:09 +0100

    Premiere sauvegarde

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Flux RSS</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="normalize.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.9/angular.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/angular-material/1.2.5/angular-material.min.css"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/angular-material/1.2.5/angular-material.min.js"></script>
+</head>
+<body>
+    <h1>Flux RSS infos de "le monde"</h1>
+    <article>
+        <?php
+            $rss_link = 'https://www.lemonde.fr/rss/en_continu.xml';
+            $rss_load = simplexml_load_file($rss_link);
+            foreach($rss_load->channel->item as $item) {
+        ?>  
+            <div class='title_container'>
+                <div>
+                    <a href='<?= $item->link ?>'><span class='title'><h3><?= $item->title ?></h3></span></a> 
+                    <span class='pubDate'><p><?= $item->pubDate?></p></span>
+                </div>
+                <div>
+                    <button mat-raised-button>Modifier</button>
+                </div>
+            </div>   
+            <div id="contenu"> 
+                <span class='description'><p><?= $item->description ?></p></span>
+            </div>
+            <hr/>
+        <?php          
+            }
+        ?>
+    </article>   
+</body>
+</html>
---
+++ modifier.php
@@ -0,0 +1,35 @@
+<?php
+  
+
+if(isset($_POST['update_buton'])){
+    $nom=$_POST['nom'];
+    $nom = addslashes($nom);
+    $synopsis=$_POST['synopsis'];
+    $synopsis = addslashes($synopsis);
+    $date=$_POST['date'];
+    $image=$_POST['image'];
+    $type=$_POST['type'];
+    $prix=$_POST['prix'];
+    
+//requete update
+    $modifier= "UPDATE films SET
+    nom = '$nom', 
+    synopsis = '$synopsis',
+    annee = '$date',
+    type = '$type', 
+    prix = $prix
+    WHERE numero = $idfilm ";
+    
+    //execute
+    $resultat=mysqli_query($conn,$modifier);   
+    //controle
+    if($resultat)
+    {
+        $message = 'La mise a jour a bien ete effectuee !';
+        header('location:films.php?sms='.$message);   
+    }
+    else{
+        die('Erreur sql !' .$resultat.'</br>'.mysql_error());    
+    }   
+}
+?>
